1f9997e03a8aeae4e0a4ae7efe3519499e6fd0bf
reginaldolauri <reginaldo.lauri@gmail.com>
2020-04-08 14:46:15 -0300

    Transformando a tabela em responsiva, criando componente de pesquisa de lançamentos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,13 @@ import {InputTextModule} from 'primeng/inputtext';
 import {ButtonModule} from 'primeng/button';
 import {TableModule} from 'primeng/table';
 import {TooltipModule} from 'primeng/tooltip';
+import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LancamentosPesquisaComponent
   ],
   imports: [
     BrowserAnimationsModule,
