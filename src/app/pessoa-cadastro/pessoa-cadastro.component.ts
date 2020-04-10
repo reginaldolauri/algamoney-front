0db490ae4b0c6111bb873f08ac0732b4bbd13589
reginaldolauri <reginaldo.lauri@gmail.com>
2020-04-10 14:06:16 -0300

    Finalizando as validações do formulário de cadastro de pessoas, fazendo tratamento de validação do botão salvar e invoando o método salvar ao clicar no botão salvar.

--- src/app/pessoa-cadastro/pessoa-cadastro.component.ts
+++ src/app/pessoa-cadastro/pessoa-cadastro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-pessoa-cadastro',
@@ -10,6 +11,10 @@ export class PessoaCadastroComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  salvar(form: NgForm) {
+    console.log('salvando pessoa...');
   }
 
 }
