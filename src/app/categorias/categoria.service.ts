f3430c74ab43fec96706c8c27bc730f9ceda2b0f
reginaldolauri <reginaldo.lauri@gmail.com>
2020-04-14 19:51:28 -0300

    Implementação do serviço de busca de categorias

---
+++ src/app/categorias/categoria.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoriaService {
+
+  categoriasUrl = `http://localhost:8080/categorias`;
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  listaTodas(): Promise<any> {
+    const headers = new HttpHeaders({
+      'Content-Type':  'application/json',
+      Authorization: 'Basic YWRtaW5AYWxnYW1vbmV5LmNvbTphZG1pbg=='
+     });
+    return this.http.get(this.categoriasUrl, { headers })
+            .toPromise()
+            .then((response: any) => {
+              return response.content;
+    });
+  }
+}
