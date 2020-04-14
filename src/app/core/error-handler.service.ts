1168a0f151bfbee3a66bb90df802569d9c18a640
reginaldolauri <reginaldo.lauri@gmail.com>
2020-04-14 19:20:04 -0300

    Implementando melhoria no tratamento de erro para mostrar a mensagem enviada pelo backend.

--- src/app/core/error-handler.service.ts
+++ src/app/core/error-handler.service.ts
@@ -13,6 +13,17 @@ export class ErrorHandlerService {
 
     if (typeof errorResponse === 'string') {
       msg = errorResponse;
+    } else if (typeof Response
+                      && errorResponse.status >= 400
+                      && errorResponse.status <= 499 ) {
+      let errors;
+      msg = 'Ocorreu um erro ao processar a sua solicitação';
+
+      try {
+        errors = errorResponse.error;
+        msg = errors[0].mensagemUsuario;
+      } catch (error) {}
+      console.error('Ocorreu um erro', errorResponse);
     } else {
       msg = 'Erro ao processar serviço remoto. Tente novamente';
       console.log('Ocorreu um erro', errorResponse);
