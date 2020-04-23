10249477eb383d37ccb4fbae7856ee6f8a941d43
reginaldolauri <reginaldo.lauri@gmail.com>
2020-04-23 17:04:38 -0300

    Ajuste para validação de sessão expirada.

--- src/app/core/error-handler.service.ts
+++ src/app/core/error-handler.service.ts
@@ -9,6 +9,8 @@ import { NotAuthenticatedError } from '../seguranca/money-http';
 })
 export class ErrorHandlerService {
 
+  sessaoExpirada = false;
+
   constructor(
     private toasty: ToastyService,
     private router: Router) { }
@@ -16,27 +18,30 @@ export class ErrorHandlerService {
   handle(errorResponse: any) {
     let msg: string;
 
-    if (typeof errorResponse === 'string') {
-      msg = errorResponse;
-    }  else if (errorResponse instanceof NotAuthenticatedError){
-      msg = 'Sua sesão expirou.';
-      this.router.navigate(['/login']);
-    } else if (typeof Response
-                      && errorResponse.status >= 400
-                      && errorResponse.status <= 499 ) {
-      let errors;
-      msg = 'Ocorreu um erro ao processar a sua solicitação';
-      if (errorResponse.status === 403) {
-        msg = 'Você não tem permissão para executar essa ação.';
+    if (!this.sessaoExpirada) {
+      if (typeof errorResponse === 'string') {
+        msg = errorResponse;
+      }  else if (errorResponse instanceof NotAuthenticatedError){
+        msg = 'Sua sesão expirou.';
+        this.sessaoExpirada = true;
+        this.router.navigate(['/login']);
+      } else if (typeof Response
+                        && errorResponse.status >= 400
+                        && errorResponse.status <= 499 ) {
+        let errors;
+        msg = 'Ocorreu um erro ao processar a sua solicitação';
+        if (errorResponse.status === 403) {
+          msg = 'Você não tem permissão para executar essa ação.';
+        }
+        try {
+          errors = errorResponse.error;
+          msg = errors[0].mensagemUsuario;
+        } catch (error) {}
+        console.error('Ocorreu um erro', errorResponse);
+      } else {
+        msg = 'Erro ao processar serviço remoto. Tente novamente';
+        console.log('Ocorreu um erro', errorResponse);
       }
-      try {
-        errors = errorResponse.error;
-        msg = errors[0].mensagemUsuario;
-      } catch (error) {}
-      console.error('Ocorreu um erro', errorResponse);
-    } else {
-      msg = 'Erro ao processar serviço remoto. Tente novamente';
-      console.log('Ocorreu um erro', errorResponse);
     }
     this.toasty.error(msg);
   }
